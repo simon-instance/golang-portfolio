25696ad386fd2ddf179c04d318b98355c83eac6f
simon <simonpeters05@gmail.com>
2020-06-29 13:02:36 +0200

    posts getall is now usable :-)

--- models/UserPost.go
+++ models/UserPost.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/scrummer123/golang-portfolio/database"
+	"google.golang.org/api/iterator"
 )
 
 // UserPost => title: title from user post, content: content from user post, userid: user id from post
@@ -14,30 +15,46 @@ type UserPost struct {
 	Content string `json:"content"`
 }
 
-var userPosts []UserPost
+var userPosts map[string]UserPost = make(map[string]UserPost)
 
 // NewUserPost Make a new user
-func NewUserPost(Title string, Content string, UserID string) UserPost {
+func NewUserPost(Title string, Content string, UserID string, DocID string) UserPost {
 	post := &UserPost{Title: Title, Content: Content, UserID: UserID}
 
-	userPosts = append(userPosts, *post)
+	userPosts[DocID] = *post
 
 	return *post
 }
 
 // GetAll returns all users
-func GetAll() []UserPost {
+func GetAll() map[string]UserPost {
 	log.SetPrefix("[models.GetAll()] :: ")
 	db := database.GetFirestoreClient()
 
-	doc, err := db.Collection("posts").Doc("7sbG4wQkEFouymAamwUA").Get(context.Background())
-	log.Printf("%v", doc.Data())
+	i := db.Collection("posts").Documents(context.Background())
+	for {
+		doc, err := i.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error occured: %v", err)
+		}
 
-	if err != nil {
-		log.Fatalf("Error occured: %v", err)
+		data := doc.Data()
+
+		uid, uidIsset := data["user_id"].(string)
+		title, titleIsset := data["title"].(string)
+		content, contentIsset := data["content"].(string)
+
+		if uidIsset && titleIsset && contentIsset {
+			post := NewUserPost(title, content, uid, doc.Ref.ID)
+
+			userPosts[doc.Ref.ID] = post
+		}
+
+		log.Printf("%v", uidIsset)
 	}
-
-	//_ := NewUserPost()
 
 	database.CloseFirestore(db)
 	return userPosts
