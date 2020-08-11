f0cecceaab98a69d707438ff8faed28e1ef80d8f
simon <simonpeters05@gmail.com>
2020-08-11 23:00:09 +0200

    made webpack config a bit cleaner

--- src/client/webpack.config.js
+++ src/client/webpack.config.js
@@ -6,40 +6,62 @@ const cssminifier = require("optimize-css-assets-webpack-plugin");
 
 const isDev = process.env.NODE_ENV === "dev";
 
+const _module = {
+  rules: [
+    {
+      test: /\.(tsx|ts)?$/,
+      use: "ts-loader",
+      exclude: /node_modules/,
+    },
+    {
+      test: /\.scss$/,
+      use: [
+        // Creates `style` nodes from JS strings
+        isDev ? "style-loader" : extracter.loader,
+        // Translates CSS into CommonJS
+        {
+          loader: "css-loader",
+          options: {
+            sourceMap: isDev,
+          },
+        },
+        // Compile sass to css
+        {
+          loader: "sass-loader",
+          options: {
+            sourceMap: isDev,
+          },
+        },
+      ],
+    },
+  ],
+};
+
+const optimization = {
+  minimize: true,
+  minimizer: [
+    new terser({
+      extractComments: true,
+    }),
+    new cssminifier({
+      filename: isDev ? "[name].css" : "[name].[hash].css",
+      chunkFilename: isDev ? "[id].css" : "[id].[hash].css",
+    }),
+  ],
+};
+
+const plugins = [
+  new hwp({
+    template: path.resolve(__dirname, "src", "app", "static", "index.html"),
+  }),
+  new extracter({ filename: "style.css" }),
+];
+
 module.exports = {
   mode: isDev ? "development" : "production",
   entry: "./src/app/index.tsx",
   devtool: "inline-source-map",
-  module: {
-    rules: [
-      {
-        test: /\.(tsx|ts)?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          // Creates `style` nodes from JS strings
-          isDev ? "style-loader" : extracter.loader,
-          // Translates CSS into CommonJS
-          {
-            loader: "css-loader",
-            options: {
-              sourceMap: isDev,
-            },
-          },
-          // Compile sass to css
-          {
-            loader: "sass-loader",
-            options: {
-              sourceMap: isDev,
-            },
-          },
-        ],
-      },
-    ],
-  },
+  module: _module,
   resolve: {
     extensions: [".tsx", ".ts", ".js", ".scss"],
   },
@@ -47,22 +69,6 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "src", "build"),
   },
-  plugins: [
-    new hwp({
-      template: path.resolve(__dirname, "src", "app", "static", "index.html"),
-    }),
-    new extracter({ filename: "style.css" }),
-  ],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new terser({
-        extractComments: true,
-      }),
-      new cssminifier({
-        filename: isDev ? "[name].css" : "[name].[hash].css",
-        chunkFilename: isDev ? "[id].css" : "[id].[hash].css",
-      }),
-    ],
-  },
+  plugins,
+  optimization,
 };
