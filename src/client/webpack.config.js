6fa2736cb38be14f869d8f3835c78e53a3639f14
simon <simonpeters05@gmail.com>
2020-08-11 22:45:45 +0200

    webpack config made better

--- src/client/webpack.config.js
+++ src/client/webpack.config.js
@@ -1,8 +1,13 @@
 const path = require("path");
 const hwp = require("html-webpack-plugin");
+const terser = require("terser-webpack-plugin");
+const extracter = require("mini-css-extract-plugin");
+const cssminifier = require("optimize-css-assets-webpack-plugin");
+
+const isDev = process.env.NODE_ENV === "dev";
 
 module.exports = {
-  mode: "development",
+  mode: isDev ? "development" : "production",
   entry: "./src/app/index.tsx",
   devtool: "inline-source-map",
   module: {
@@ -13,20 +18,30 @@ module.exports = {
         exclude: /node_modules/,
       },
       {
-        test: /\.s[ac]ss$/i,
+        test: /\.scss$/,
         use: [
           // Creates `style` nodes from JS strings
-          "style-loader",
+          isDev ? "style-loader" : extracter.loader,
           // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: isDev,
+            },
+          },
+          // Compile sass to css
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: isDev,
+            },
+          },
         ],
       },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: [".tsx", ".ts", ".js", ".scss"],
   },
   output: {
     filename: "bundle.js",
@@ -36,5 +51,18 @@ module.exports = {
     new hwp({
       template: path.resolve(__dirname, "src", "app", "static", "index.html"),
     }),
+    new extracter({ filename: "style.css" }),
   ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new terser({
+        extractComments: true,
+      }),
+      new cssminifier({
+        filename: isDev ? "[name].css" : "[name].[hash].css",
+        chunkFilename: isDev ? "[id].css" : "[id].[hash].css",
+      }),
+    ],
+  },
 };
